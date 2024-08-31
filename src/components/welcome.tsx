bd3a4ab979d0ae70d4dc216d0d7a2debe7d84fea
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-09-01 00:01:23 +0200

    🐫 Update homepage

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -3,18 +3,17 @@ export default function Welcome() {
     <div className="bg-dark-green-default">
       <div className="flex h-screen flex-col items-center justify-center gap-5 text-light-green-default">
         <div className="-mt-20 flex flex-col gap-y-2 text-center">
-          <h3 className="uppercase xl:text-4xl 2xl:text-4xl xs:text-[19px]">
+          <h3 className="uppercase xl:text-4xl 2xl:text-4xl xs:text-base">
             Welcome to our fellowship
           </h3>
           <h1 className="font-reimbrandt text-[14vw]">Perki Aachen</h1>
-          <div className="flex gap-20 xs:gap-4">
-            <div className="flex items-center gap-2 uppercase xl:text-4xl 2xl:text-4xl xs:text-[0.8rem]">
+          <div className="flex justify-between gap-20 xs:gap-4 xs:text-sm">
+            <div className="flex items-center gap-2 uppercase xl:text-4xl 2xl:text-4xl">
               <p>since</p>
-              <div className="h-[0.1vw] w-[7vw] bg-light-green-default"></div>
               <p>1990</p>
             </div>
-            <div className="uppercase xl:text-4xl 2xl:text-4xl xs:text-[0.8rem]">
-              Non-denominational church
+            <div className="uppercase xl:text-4xl 2xl:text-4xl">
+              Non-denominational
             </div>
           </div>
         </div>
