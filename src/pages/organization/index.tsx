e3e8f5edb7375e4063a5971f5980d4a85fa62a5c
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-09-01 11:42:27 +0200

    🐏 Add organization page

--- src/pages/organization/index.tsx
+++ src/pages/organization/index.tsx
@@ -1,3 +1,100 @@
+import Template from "~/components/template";
+
 export default function Organization() {
-  return <div></div>;
+  return (
+    <Template title="Org. Chart" subtitle="Our family structure">
+      <h1 className="absolute top-[50%] animate-pulse font-reimbrandt text-4xl">
+        Coming Soon ...
+      </h1>
+      {/* <div className="overflow-x-scroll">
+        <Tree
+          lineWidth={"2px"}
+          lineColor={"#d7dfcb"}
+          lineBorderRadius={"10px"}
+          label={<Button>MitA</Button>}
+        >
+          <TreeNode label={<Button>Internal</Button>}>
+            <TreeNode
+              label={
+                <Button className="w-fit whitespace-nowrap text-sm">
+                  Clarrisa A.
+                </Button>
+              }
+            />
+            <TreeNode
+              label={
+                <Button className="w-fit whitespace-nowrap text-sm">
+                  Reggy I.
+                </Button>
+              }
+            />
+            <TreeNode
+              label={
+                <Button className="w-fit whitespace-nowrap text-sm">
+                  Winston A.
+                </Button>
+              }
+            />
+            <TreeNode
+              label={
+                <Button className="w-fit whitespace-nowrap text-sm">
+                  Toni S.
+                </Button>
+              }
+            />
+            <TreeNode
+              label={
+                <Button className="w-fit whitespace-nowrap text-sm">
+                  Shane A.
+                </Button>
+              }
+            />
+          </TreeNode>
+          <TreeNode
+            label={
+              <Button className="w-fit whitespace-nowrap text-sm">
+                External
+              </Button>
+            }
+          >
+            <TreeNode
+              label={
+                <Button className="w-fit whitespace-nowrap text-sm">
+                  Felix A.
+                </Button>
+              }
+            />
+            <TreeNode
+              label={
+                <Button className="w-fit whitespace-nowrap text-sm">
+                  Oliver R.
+                </Button>
+              }
+            />
+            <TreeNode
+              label={
+                <Button className="w-fit whitespace-nowrap text-sm">
+                  Danny K.
+                </Button>
+              }
+            />
+            <TreeNode
+              label={
+                <Button className="w-fit whitespace-nowrap text-sm">
+                  Victor J.
+                </Button>
+              }
+            />
+            <TreeNode
+              label={
+                <Button className="w-fit whitespace-nowrap text-sm">
+                  Ido L.
+                </Button>
+              }
+            />
+          </TreeNode>
+        </Tree>
+      </div> */}
+    </Template>
+  );
 }
